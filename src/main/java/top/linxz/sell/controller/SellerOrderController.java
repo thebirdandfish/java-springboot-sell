2e40fbd1954e9e16ca978e9b37bdb7a31d29cf28
linxinzhe <linxinzhechn@qq.com>
2018-03-28 09:53:30 +0800

    feat(seller order controller): list order cancel

--- src/main/java/top/linxz/sell/controller/SellerOrderController.java
+++ src/main/java/top/linxz/sell/controller/SellerOrderController.java
@@ -1,5 +1,6 @@
 package top.linxz.sell.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -9,12 +10,15 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import top.linxz.sell.dto.OrderDTO;
+import top.linxz.sell.enums.ResultEnum;
+import top.linxz.sell.exception.SellException;
 import top.linxz.sell.service.OrderService;
 
 import java.util.Map;
 
 @Controller
 @RequestMapping("/seller/order")
+@Slf4j
 public class SellerOrderController {
     @Autowired
     private OrderService orderService;
@@ -29,5 +33,25 @@ public class SellerOrderController {
         map.put("currentPage", page);
         map.put("size", size);
         return new ModelAndView("order/list", map);
+    }
+
+    @GetMapping("/cancel")
+    public ModelAndView cancel(@RequestParam("orderId") String orderId,
+                               Map<String, Object> map) {
+        try {
+            OrderDTO orderDTO = orderService.findOne(orderId);
+            orderService.cancel(orderDTO);
+        } catch (SellException e) {
+            log.error("【卖家端取消订单】发生异常{}", e);
+            map.put("msg", e.getMessage());
+            map.put("url", "/sell/seller/order/list");
+
+            return new ModelAndView("common/error", map);
+
+        }
+
+        map.put("msg", ResultEnum.ORDER_CANCEL_SUCCESS.getMessage());
+        map.put("url", "/sell/seller/order/list");
+        return new ModelAndView("common/success");
     }
 }
